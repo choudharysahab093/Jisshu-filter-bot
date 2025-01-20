9193055834b8b40aafb1a40070aaba5d088c154b
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2025-01-20 10:45:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7745762451:AAGE37KmSTK-SkTCJc-Dhoc-BxIwnMv
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7977862043').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002282451650'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002281424624'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviegroup255')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002087242344').split()]
 #---------------------------------------------------------------
